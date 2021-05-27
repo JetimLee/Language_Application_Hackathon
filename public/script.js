77fd3e752dbab4955fd6835a30f09d5ca0c1009f
Josh Regev <jsrgev@gmail.com>
2021-05-27 14:12:36 +0300

    selects words for user and displays card and checks if answer correct

--- public/script.js
+++ public/script.js
@@ -1,4 +1,11 @@
-let language_words = [{word_id:1,language_id:1,word:"un",translation:"one",part_of_speech:"number"},{word_id:2,language_id:1,word:"deux",translation:"two",part_of_speech:"number"},{word_id:3,language_id:1,word:"trois",translation:"three",part_of_speech:"number"},{word_id:4,language_id:1,word:"chien",translation:"dog",part_of_speech:"noun"},{word_id:5,language_id:1,word:"chat",translation:"cat",part_of_speech:"noun"},{word_id:6,language_id:2,word:"uno",translation:"one",part_of_speech:"number"}];
+let language_words = [
+	{word_id:1,language_id:1,word:"un",translation:"one",part_of_speech:"number"},
+	{word_id:2,language_id:1,word:"deux",translation:"two",part_of_speech:"number"},
+	{word_id:3,language_id:1,word:"trois",translation:"three",part_of_speech:"number"},
+	{word_id:4,language_id:1,word:"chien",translation:"dog",part_of_speech:"noun"},
+	{word_id:5,language_id:1,word:"chat",translation:"cat",part_of_speech:"noun"},
+	{word_id:6,language_id:2,word:"uno",translation:"one",part_of_speech:"number"}
+	];
 
 let languages = [{language_id:1,language_name:"french"},{language_id:2,language_name:"spanish"}];
 
@@ -16,25 +23,59 @@ const showLanguageList = () => {
 }
 showLanguageList();
 
-let userWords = [{user_id:"1"},{language_id:"1"},{word_id:"1"},{times_right:0},{times_wrong:0,},
-{user_id:"1"},{language_id:"1"},{word_id:"2"},{times_right:0},{times_wrong:0,},
-{user_id:"2"},{language_id:"1"},{word_id:"1"},{times_right:0},{times_wrong:0,}]
+let userWords =[
+{user_id:1,language_id:1,word_id:1,times_right:0,times_wrong:0},
+{user_id:1,language_id:1,word_id:2,times_right:0,times_wrong:0},
+{user_id:2,language_id:1,word_id:1,times_right:0,times_wrong:0}
+]
 
 let thisUser = 1;
 
-let sessionWords = (user_id) => {
-	console.log(userWords[0]);
-	let words = userWords.filter(a => a.user_id == user_id);
-	return words;
+let sessionWords;
+
+let getSessionWords = (user_id) => {
+	sessionWords = userWords.filter(a => a.user_id == user_id);
 }
 
-console.log(sessionWords(thisUser));
+getSessionWords(thisUser);
 
-const flashcard = () => {
-	let car = document.createElement("div");
-	div.classList.add("card");
+let currentCard;
+
+setCurrentCard = (word) => {
+	let matches = language_words.filter(a => a.word_id === word.word_id);
+	currentCard = { word_id:matches[0].word_id, word:matches[0].word, translation: matches[0].translation};
 }
 
+setCurrentCard(sessionWords[1]);
+
+const checkAnswer = (string) => {
+	if (string == currentCard.translation) {
+		console.log("Correct");
+		// userWords
+	} else {
+		console.log("Wrong")
+	}
+}
+
+const submitAnswer = (event) => {
+	if (event.key == "Enter") {
+		checkAnswer(event.target.value);
+	}
+}
+
+const showCard = () => {
+	let card = document.createElement("div");
+	card.classList.add("card");
+	card.innerHTML = `
+	<div>${currentCard.word}</div>
+	<div><input id="answer" type="text"></div>
+	`;
+	root.appendChild(card);
+	let answer = document.getElementById("answer");
+	answer.addEventListener("keyup",submitAnswer)
+}
+
+showCard();
 
 
 

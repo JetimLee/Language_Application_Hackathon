04082e8dc339673a16ea32f0e3aa59d51da39145
Josh Regev <jsrgev@gmail.com>
2021-05-27 16:48:24 +0300

    selection menus working

--- public/script.js
+++ public/script.js
@@ -7,27 +7,73 @@ let language_words = [
 	{word_id:6,language_id:2,word:"uno",translation:"one",part_of_speech:"number"}
 	];
 
-let languages = [{language_id:1,language_name:"french"},{language_id:2,language_name:"spanish"}];
-
-let root = document.getElementById("root");
-
-const showLanguageList = () => {
-	let select = document.createElement("select");
-	for (let item of languages) {
-		let option = document.createElement("option");
-		option.setAttribute("value",item.language_id);
-		option.textContent = item.language_name;
-		select.appendChild(option);
-	}
-	root.appendChild(select)
-}
-showLanguageList();
+let languages = [
+{language_id:1,language_name:"french"},
+{language_id:2,language_name:"spanish"},
+{language_id:3,language_name:"italian"},
+{language_id:4,language_name:"japanese"},
+];
 
 let userWords =[
 {user_id:1,language_id:1,word_id:1,times_right:0,times_wrong:0},
 {user_id:1,language_id:1,word_id:2,times_right:0,times_wrong:0},
+{user_id:1,language_id:2,word_id:1,times_right:0,times_wrong:0},
 {user_id:2,language_id:1,word_id:1,times_right:0,times_wrong:0}
 ]
+
+let root = document.getElementById("root");
+
+let userLanguagesIds;
+let otherLanguagesIds;
+let allLanguagesIds = languages.map(a => a.language_id);
+let currentLanguage;
+
+const capitalize = (string) => {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+const selectLanguage = (event) => {
+	if (event.target.value >= 0) {
+		currentLanguage = event.target.value;
+	}
+}
+
+const showLanguageList = (ids = allLanguagesIds) => {
+	let select = document.createElement("select");
+	let subsetLanguages = languages.filter(a => ids.includes(a.language_id));
+	let option = document.createElement("option");
+		// option.setAttribute();
+		option.textContent = "Select one";
+		select.appendChild(option);
+	for (let item of subsetLanguages) {
+		let option = document.createElement("option");
+		option.setAttribute("value",item.language_id);
+		option.textContent = capitalize(item.language_name); 
+		select.appendChild(option);
+	}
+	select.addEventListener("change",selectLanguage)
+	root.appendChild(select);
+}
+
+const startScreen = () => {
+	let userWordsArray = userWords.map(a=>a.language_id);
+	userLanguagesIds = allLanguagesIds.filter(a => userWordsArray.includes(a));
+	otherLanguagesIds = allLanguagesIds.filter(a => !userWordsArray.includes(a));
+	root.innerHTML = "";
+	let div1 = document.createElement("div");
+	div1.textContent = "Welcome! Choose one of your languages to practice:"
+	root.appendChild(div1);
+	showLanguageList(userLanguagesIds);
+	let div2 = document.createElement("div");
+	div2.textContent = "Or you can choose a new language:"
+	root.appendChild(div2);
+	showLanguageList(otherLanguagesIds);
+}
+
+startScreen();
+
+
+
 
 let thisUser = 1;
 
@@ -46,14 +92,16 @@ setCurrentCard = (word) => {
 	currentCard = { word_id:matches[0].word_id, word:matches[0].word, translation: matches[0].translation};
 }
 
-setCurrentCard(sessionWords[1]);
+setCurrentCard(sessionWords[0]);
 
 const checkAnswer = (string) => {
+	index = userWords.findIndex(a => a.word_id == currentCard.word_id);
 	if (string == currentCard.translation) {
 		console.log("Correct");
-		// userWords
+		userWords[index].times_right++;
 	} else {
 		console.log("Wrong")
+		userWords[index].times_wrong++;
 	}
 }
 
